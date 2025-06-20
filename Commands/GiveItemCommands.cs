c7a10eaf67b011612a354ddaaf558ad2a9dec519
lakeff <163956194+lakeff@users.noreply.github.com>
2025-06-20 16:59:35 -0300

    Update GiveItemCommands.cs

--- Commands/GiveItemCommands.cs
+++ Commands/GiveItemCommands.cs
@@ -1,17 +1,1 @@
-using KindredCommands.Commands.Converters;
-using ProjectM;
-using VampireCommandFramework;
 
-namespace KindredCommands.Commands;
-
-internal class GiveItemCommands
-{
-	[Command("give", "g", "<Prefab GUID or name> [quantity=1]", "Gives the specified item to the player", adminOnly: true)]
-	public static void GiveItem(ChatCommandContext ctx, ItemParameter item, int quantity = 1)
-	{
-		Helper.AddItemToInventory(ctx.Event.SenderCharacterEntity, item.Value, quantity);
-		var prefabSys = Core.Server.GetExistingSystemManaged<PrefabCollectionSystem>();
-		var name = prefabSys._PrefabLookupMap.GetName(item.Value);
-		ctx.Reply($"Gave {quantity} {name}");
-	}
-}
